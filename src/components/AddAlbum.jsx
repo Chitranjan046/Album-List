ff01e208222aa70246d273efff91c7c473879803
Chitranjan046 <chitranjankumarpatel8@gmail.com>
2023-09-19 11:44:53 +0530

    Another Approach

--- src/components/AddAlbum.jsx
+++ src/components/AddAlbum.jsx
@@ -1,50 +1,41 @@
-import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
+import React from 'react'
+import { Link } from "react-router-dom";
 import Navbar from './Navbar';
 import './AddAlbum.css';
 
-const AddAlbum = ({ addAlbumToList }) => {
-  const [userId, setUserId] = useState('');
-  const [title, setTitle] = useState('');
+const AddAlbum = (props) => {
 
-  const handleAddClick = () => {
-    addAlbumToList(Number(userId), title);
-  };
+  //this function get all the input like userid and title then call add albumm function for add it on the album list
+  const getAlbumFormData = () => {
+    const userId = document.getElementById('aaform-userid-inp').value;
+    const title = document.getElementById('aaform-title-inp').value;
+    props.addAlbumToList(Number(userId), title)
+  }
 
   return (
     <>
-      {/* Navbar */}
+      {/* navber */}
       <Navbar path="/" page="Home" />
 
-      <div className="addalbum-container">
-        <div className="addalbum-form">
+
+      <div className='addalbum-container'>
+        <div className='addalbum-form'>
           <h4>Now Enter A New Album Details</h4>
-          <div className="inp-container">
-            User Id :
-            <input
-              type="number"
-              placeholder="Enter User Id"
-              value={userId}
-              onChange={(e) => setUserId(e.target.value)}
-            />
+          <div className='inp-container'>
+             User Id :
+            <input id='aaform-userid-inp' type="number" placeholder='Enter User Id' />
           </div>
-          <div className="inp-container">
-            Album Title :
-            <input
-              type="text"
-              placeholder="Enter Album Title"
-              value={title}
-              onChange={(e) => setTitle(e.target.value)}
-            />
+          <div className='inp-container'>
+             Album Title :
+            <input id='aaform-title-inp' type="text" placeholder='Enter Album Title' />
           </div>
           <div>
-            <button onClick={handleAddClick}>Adding To List</button>
-            <Link to="/">Cancel</Link>
+            <Link to="/"><button onClick={getAlbumFormData}>Adding To List</button></Link>
           </div>
         </div>
       </div>
     </>
-  );
-};
+  )
+}
 
-export default AddAlbum;
+export default AddAlbum
